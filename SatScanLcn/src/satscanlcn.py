cffb2a36fa9bcd03006a89859b6fc6a7cecce437
Huevos <rowland@moralet.com>
2021-10-30 22:01:28 +0200

    Tweak regions code

--- SatScanLcn/src/satscanlcn.py
+++ SatScanLcn/src/satscanlcn.py
@@ -134,8 +134,9 @@ class SatScanLcn(Screen): # the downloader
 		
 		if getattr(self.config, self.config.provider.value, None): # check if there is a regions ConfigSelection for this provider
 			bat_regions = getattr(self.config, self.config.provider.value)
-			self.bat_BouquetID = bat_regions.value[0]
-			self.bat_region = bat_regions.value[1]
+			if "bat" in PROVIDERS[self.config.provider.value] and "bat_regions" in PROVIDERS[self.config.provider.value]["bat"] and bat_regions.value in PROVIDERS[self.config.provider.value]["bat"]["bat_regions"]:
+				self.bat_BouquetID = PROVIDERS[self.config.provider.value]["bat"]["bat_regions"][bat_regions.value][0]
+				self.bat_region = PROVIDERS[self.config.provider.value]["bat"]["bat_regions"][bat_regions.value][1]
 
 		if self.bat_lcn_descriptor:
 			self.descriptors["lcn"] = self.bat_lcn_descriptor
